345f09aed4000093324303bdd6b0e5e8a9ed4586
canales16 <vari_14@hotmail.com>
2013-07-30 20:10:02 -0600

    Primer Commit

---
+++ progra4-minidropbox-master/MiniDropbox.Domain/Entities/Account.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MiniDropbox.Domain
+{
+    public class Account : IEntity
+    {
+        public virtual long Id { get; set; }
+        public virtual bool IsArchived { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual IList<File> Files { get; set; }
+    }
+}
---
+++ progra4-minidropbox-master/MiniDropbox.Web/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using BootstrapMvcSample.Controllers;
+using MiniDropbox.Web.Models;
+
+namespace MiniDropbox.Web.Controllers
+{
+    public class AccountController : BootstrapBaseController
+    {
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return View(new AccountLoginModel());
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(AccountLoginModel model)
+        {
+            return RedirectToAction("ListAllContent", "Disk");
+        }
+    }
+}
